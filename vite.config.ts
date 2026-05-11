f59a4da0aec7794bdb75917ea5313ccf8bbc63e3
manjunadha12 <dmanjunadha06@gmail.com>
2026-05-11 10:14:13 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/web/",
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': 'http://localhost:4000',
-    },
-  },
-})
+});
